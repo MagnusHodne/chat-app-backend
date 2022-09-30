4c59d4bb377dd78e19c8d9716b84bea05892672d
Magnus H <me@magnushodne.com>
2022-09-30 15:06:53 +0200

    Stubbed out server

---
+++ src/main/kotlin/com/magnushodne/chatapp/controller/ChatController.kt
@@ -0,0 +1,22 @@
+package com.magnushodne.chatapp.controller
+
+import com.magnushodne.chatapp.model.Message
+import jdk.jshell.spi.ExecutionControl.NotImplementedException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/api/v1/chats")
+class ChatController {
+
+    @GetMapping("/")
+    fun getAllChats(): ResponseEntity<List<Message>>{
+        return ResponseEntity.ok().body(ArrayList())
+    }
+
+    @DeleteMapping("/")
+    fun deleteOne(): ResponseEntity.BodyBuilder {
+        throw NotImplementedException("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/com/magnushodne/chatapp/model/Message.kt
@@ -0,0 +1,5 @@
+package com.magnushodne.chatapp.model
+
+class Message {
+
+}
