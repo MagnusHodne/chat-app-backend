cf6005fe9a17a2bab7ba9a88223010f73f00ee68
Magnus Hodne <magnus@drobak.org>
2022-10-04 16:00:59 +0200

    SecurityConfig includes Jwt decoding

--- src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
+++ src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
@@ -1,13 +1,35 @@
 package com.magnushodne.chatapp.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
 
-
+@Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+    @Value("\${auth0.audience}")
+    private val audience: String? = null
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private val issuer: String? = null
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder? {
+        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation<JwtDecoder>(issuer) as NimbusJwtDecoder
+        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience!!)
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
+        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
+        jwtDecoder.setJwtValidator(withAudience)
+        return jwtDecoder
+    }
+
+
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
