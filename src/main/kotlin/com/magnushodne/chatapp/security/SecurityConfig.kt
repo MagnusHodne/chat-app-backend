a20928751c142a84930ee0dcbae95c1578ff5518
Magnus H <me@magnushodne.com>
2022-10-05 18:44:49 +0200

    Stripped back server to bare minimum configuration for auth0

--- src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
+++ src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
@@ -13,23 +13,6 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
-
-    @Value("\${auth0.audience}")
-    private val audience: String? = null
-    @Value("\${auth0.domain}")
-    private val issuer: String? = null
-
-    @Bean
-    fun jwtDecoder(): JwtDecoder? {
-        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation<JwtDecoder>(issuer) as NimbusJwtDecoder
-        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience!!)
-        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
-        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
-        jwtDecoder.setJwtValidator(withAudience)
-        return jwtDecoder
-    }
-
-
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
