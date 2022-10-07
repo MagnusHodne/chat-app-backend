8f37bb51791e10b09df8a280b16de19c03bd2eee
Magnus Hodne <magnus@drobak.org>
2022-10-07 14:12:44 +0200

    Briefly commented class

--- src/main/kotlin/com/magnushodne/chatapp/security/AudienceValidator.kt
+++ src/main/kotlin/com/magnushodne/chatapp/security/AudienceValidator.kt
@@ -5,7 +5,7 @@ import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
 import org.springframework.security.oauth2.jwt.Jwt
 
-// TODO - document what this class does
+// Checks whether the token is actually meant for the API it's requesting an "audience" for
 internal class AudienceValidator(private val audience: String) : OAuth2TokenValidator<Jwt> {
     override fun validate(jwt: Jwt): OAuth2TokenValidatorResult {
         val error = OAuth2Error("invalid_token", "The required audience is missing", null)
