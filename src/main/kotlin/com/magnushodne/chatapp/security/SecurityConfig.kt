a7d45f9870d7e7fb7c35d9ab5b35cdd467c6fd1d
Magnus H <me@magnushodne.com>
2022-10-09 20:08:49 +0200

    Added jwtDecoder

--- src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
+++ src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
@@ -13,6 +13,24 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+    @Value("\${auth0.audience}")
+    private lateinit var audience: String
+
+    @Value("\${auth0.domain}")
+    private lateinit var issuer: String
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder? {
+        //Get a decoder from our issuer (Auth0)
+        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation<JwtDecoder>(issuer) as NimbusJwtDecoder
+        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience)
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
+        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
+        jwtDecoder.setJwtValidator(withAudience)
+        return jwtDecoder
+    }
+
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
