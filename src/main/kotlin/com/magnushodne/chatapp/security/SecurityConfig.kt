49c7a89c2eaed5d01ea869a8b1bcdc73450f2752
Magnus Hodne <magnus@drobak.org>
2022-10-12 13:59:27 +0200

    Updated SecurityConfig to handle cors

--- src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
+++ src/main/kotlin/com/magnushodne/chatapp/security/SecurityConfig.kt
@@ -9,17 +9,53 @@ import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+    @Value("\${auth0.audience}")
+    private val audience: String? = null
+    @Value("\${auth0.domain}")
+    private val issuer: String? = null
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder? {
+        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation<JwtDecoder>(issuer) as NimbusJwtDecoder
+        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience!!)
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
+        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
+        jwtDecoder.setJwtValidator(withAudience)
+        return jwtDecoder
+    }
+
+
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeRequests()
-            .mvcMatchers("/").permitAll()
+        http.cors()
+            .configurationSource(corsConfigurationSource()).and()
+            .authorizeRequests()
             .anyRequest().authenticated().and()
-            .oauth2Login()
+            .oauth2ResourceServer().jwt()
+
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(
+            "http://localhost:5000/",
+        )
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = Collections.singletonList("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
