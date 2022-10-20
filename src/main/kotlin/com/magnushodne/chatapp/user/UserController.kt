28e8ddfe503c1700fa04f2965bf808c39f692e67
Magnus H <me@magnushodne.com>
2022-10-20 16:09:02 +0200

    Updated endpoint url to conform with plural naming convention

--- src/main/kotlin/com/magnushodne/chatapp/user/UserController.kt
+++ src/main/kotlin/com/magnushodne/chatapp/user/UserController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 class UserController(@Autowired private val userService: UserService) {
 
     @PostMapping("/login")
