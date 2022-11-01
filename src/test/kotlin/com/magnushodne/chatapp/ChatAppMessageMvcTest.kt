4da678ba36f9e235376d4c0273d44af56128344f
Magnus H <me@magnushodne.com>
2022-11-01 23:38:23 +0100

    Initialising test database via sql script

--- src/test/kotlin/com/magnushodne/chatapp/ChatAppMessageMvcTest.kt
+++ src/test/kotlin/com/magnushodne/chatapp/ChatAppMessageMvcTest.kt
@@ -1,16 +1,16 @@
 package com.magnushodne.chatapp
 
 import org.json.JSONObject
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.post
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ChatAppMessageMvcTest(@Autowired private val mockMvc: MockMvc) {
 
     val baseUrl = "http://localhost:5000/api/v1/chats"
@@ -46,6 +47,7 @@ class ChatAppMessageMvcTest(@Autowired private val mockMvc: MockMvc) {
     }
 
     @Test
+    @Sql("/database/insert_one_of_each_entity.sql")
     fun postMessage_withValidJwtToken_returnsCreated() {
         val messagePayload = JSONObject().put("content", "Hello world").put("channelId", 1).put("authorId", 1)
 
---
+++ src/test/resources/database/insert_one_of_each_entity.sql
@@ -0,0 +1,3 @@
+insert into users(sub, username, picture, description) values('user', 'testuser', '', 'description');
+insert into servers(owner_id, name, description) values (1, 'testserver', 'description');
+insert into channels(server_id, name, description) values (1, 'testchannel', 'description');
