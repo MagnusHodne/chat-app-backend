fae3debb571a1b1383edb966b721d7605fe9a59f
Magnus H <me@magnushodne.com>
2022-11-01 17:54:05 +0100

    CRUD methods for MessageService

--- src/main/kotlin/com/magnushodne/chatapp/message/MessageService.kt
+++ src/main/kotlin/com/magnushodne/chatapp/message/MessageService.kt
@@ -1,13 +1,36 @@
 package com.magnushodne.chatapp.message
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class MessageService(@Autowired private val messageRepository: MessageRepository) {
 
+    fun createMessage(message: MessageEntity): MessageEntity {
+        return messageRepository.save(message)
+    }
+
+    fun getMessageById(id: Long): MessageEntity? {
+        return messageRepository.findByIdOrNull(id)
+    }
+
     fun getAllMessages(): List<MessageEntity> {
         return messageRepository.findAll()
+    }
+
+    fun updateMessage(message: MessageEntity): MessageEntity? {
+        message.id?.let {
+            return messageRepository.save(message)
+        }
+        return null
+    }
+
+    fun deleteOneMessage(id: Long): Boolean {
+        val message = messageRepository.findByIdOrNull(id)
+        message?.let { messageRepository.delete(it)
+            return true
+        }?: return false
     }
 }
 
