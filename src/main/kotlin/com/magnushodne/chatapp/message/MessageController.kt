0d98bf5421a261ae7f787f665c985f3e628abc2e
Magnus H <me@magnushodne.com>
2022-11-01 18:01:21 +0100

    CRUD methods for controller

--- src/main/kotlin/com/magnushodne/chatapp/message/MessageController.kt
+++ src/main/kotlin/com/magnushodne/chatapp/message/MessageController.kt
@@ -3,22 +3,35 @@ package com.magnushodne.chatapp.message
 import jdk.jshell.spi.ExecutionControl.NotImplementedException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/chats")
 class MessageController(@Autowired private val messageService: MessageService) {
 
-    @GetMapping
-    fun getAllChats(): ResponseEntity<String>{
-        return ResponseEntity.ok().body("This will be a list of messages")
+    @PostMapping
+    fun createMessage(@RequestBody message: MessageEntity): ResponseEntity<MessageEntity> {
+        return ResponseEntity.ok(messageService.createMessage(message))
     }
 
-    @DeleteMapping
-    fun deleteOne(): ResponseEntity.BodyBuilder {
-        throw NotImplementedException("Not yet implemented")
+    @GetMapping("/{id}")
+    fun getOneMessage(@PathVariable id: Long): ResponseEntity<MessageEntity?> {
+        return ResponseEntity.ok(messageService.getMessageById(id))
     }
+
+    @GetMapping
+    fun getAllChats(): ResponseEntity<List<MessageEntity>>{
+        return ResponseEntity.ok(messageService.getAllMessages())
+    }
+
+    @PutMapping("/{id}")
+    fun updateMessage(@PathVariable id: Long, @RequestBody message: MessageEntity): ResponseEntity<MessageEntity?> {
+        return ResponseEntity.ok(messageService.updateMessage(message))
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteOneMessage(@PathVariable id: Long): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(messageService.deleteOneMessage(id))
+    }
+
 }
