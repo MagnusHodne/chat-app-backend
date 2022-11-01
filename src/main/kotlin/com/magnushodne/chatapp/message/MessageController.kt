7c563d68a8d7db3d832a8831ad5686b053d83ce0
Magnus H <me@magnushodne.com>
2022-11-01 22:12:43 +0100

    postMessage_withValidJwtToken_returnsCreated 🟢

--- src/main/kotlin/com/magnushodne/chatapp/message/MessageController.kt
+++ src/main/kotlin/com/magnushodne/chatapp/message/MessageController.kt
@@ -4,6 +4,8 @@ import com.magnushodne.chatapp.dtos.NewMessageDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/v1/chats")
@@ -11,7 +13,8 @@ class MessageController(@Autowired private val messageService: MessageService) {
 
     @PostMapping
     fun createMessage(@RequestBody message: NewMessageDto): ResponseEntity<MessageEntity> {
-        return ResponseEntity.ok(messageService.createMessage(message))
+        val uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/chats").toUriString())
+        return ResponseEntity.created(uri).body(messageService.createMessage(message))
     }
 
     @GetMapping("/{id}")
