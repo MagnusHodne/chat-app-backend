868dd554bdb8a2061ac9ebed830650360dd5eea4
Magnus H <me@magnushodne.com>
2022-11-01 21:36:57 +0100

    Created basic context loading test

---
+++ src/test/kotlin/com/magnushodne/chatapp/ChatAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.magnushodne.chatapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ChatAppApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
